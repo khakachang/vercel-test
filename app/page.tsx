587fd874120a02e37189d762331797d14dbbae95
Khakachang Tripura <khakachang22@gmail.com>
2026-01-08 00:36:02 +0530

    Added my Github Profile Link

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,9 @@ export default function Test(){
   return <>
   
   <h1 className="text-center font-bold text-4xl p-3">Hello There</h1>
+  <div className="box-border size-32 border-4 p-4  m-auto bg-red-50">
+    <h3>Follow Me on Github</h3>
+  <a href="https://github.com/khakachang"><button className="btn btn-primary">Follow</button></a>
+  </div>
   </>
 }
